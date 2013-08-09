4c00f8b590b1b8fc12740d80187e82fc028295f0
Alice Lee <ojamarushealice@yahoo.com>
2013-08-09 14:24:06 -0400

    Finish create_user, generate token, validate token

--- db.php
+++ db.php
@@ -13,29 +13,42 @@ function db() {
 function create_user($username, $password, $icon = null) {
     $db = db();
 
-    $password = sha1
+    $password = sha1($password);
 
+    $query = "SELECT 1 FROM user WHERE username = '".mysqli_real_escape_string($db, $username)."'";
+    $result = mysqli_query($db, $query);
+    if (mysqli_num_rows($result)) {
+        return array("error"=>"Username is already taken"); 
+    }
 
     $query = "INSERT INTO user (username, password, icon) VALUES
-                ('".mysqli_real_escape_string($username)."',
-                 '".mysqli_real_escape_string($password)."',
-                 '".mysqli_real_escape_string($icon)."')";
+                ('".mysqli_real_escape_string($db, $username)."',
+                 '".mysqli_real_escape_string($db, $password)."',
+                 '".mysqli_real_escape_string($db, $icon)."')";
 
-    $result = mysqli_query($query, $db);
+    $result = mysqli_query($db, $query);
+    if(mysqli_affected_rows($db) != 1){
+        error_log("Create user failed: ".mysqli_error($db));
+        return false;
+    }
+    
+    $userid = mysqli_insert_id($db);
+    
+    $token = generate_token($userid, $password);
 
+    return array('id'=>$userid, 'token'=>$token);
 }
-
 
 function create_post($author, $body, $private = 0, $replyto = null, $replyparent = null) {
     $db = db();
 
     $query = "INSERT INTO messages (author, body, private, replyto, replyparent) VALUES
-        ('".mysqli_real_escape_string($author)."',
-         '".mysqli_real_escape_string($body)."',
-         '".mysqli_real_escape_string($private)."',
-         '".mysqli_real_escape_string($replyto)."',
-         '".mysqli_real_escape_string($replyparent)."')";
-    $result = mysqli_query($query, $db) or error_log(mysqli_error($db));
+        ('".mysqli_real_escape_string($db, $author)."',
+         '".mysqli_real_escape_string($db, $body)."',
+         '".mysqli_real_escape_string($db, $private)."',
+         '".mysqli_real_escape_string($db, $replyto)."',
+         '".mysqli_real_escape_string($db, $replyparent)."')";
+    $result = mysqli_query($db, $query) or error_log(mysqli_error($db));
 
     // TODO: Insert into follower queues
 
@@ -43,6 +56,24 @@ function create_post($author, $body, $private = 0, $replyto = null, $replyparent
 }
 
 
+function generate_token($id, $pass) {
+    $token = sha1($id.$pass.SECRET);
+    return $token;
+}
 
+
+function validate_token($id, $token) {
+    $db = db();
+    $query = "SELECT password FROM user WHERE id = '".mysqli_real_escape_string($db, $id)."'";
+    $result = mysqli_query($db, $query);
+    if(mysqli_num_rows($result) == 1){
+        $password = mysqli_fetch_row($result)[0];
+        $validtoken = generate_token($id, $password);
+        if ($token === $validtoken) {
+            return true;
+        }
+    }
+    return array("error"=> "Invalid Token");
+}
 
 ?>
