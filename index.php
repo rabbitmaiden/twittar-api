5ed5ce04d378ab9d8de4f629b71749764e7c8afd
aliceandstuff <204740@Alice-Rehfeldt-7877.local>
2013-08-14 12:37:24 -0400

    Add skeleton of api

--- index.php
+++ index.php
@@ -8,9 +8,35 @@
 
 
     if ($method == 'login') {
+        $username = input('username');
+        $password = input('password');
+        if (empty($username) || empty($password)) {
+            fatal("Invalid username/password provided");
+        }
 
+ 
     }else if ($method == 'create_user') {
+        $username = input('username');
+        $password = input('password');
+        if (empty($username) || empty($password)) {
+            fatal("Invalid username/password provided");
+        }
 
+        output(create_user($username, $password));
+    }else if ($method == 'create_post') {
+        $user = require_login();
+    }else if ($method == 'delete_post') {
+        $user = require_login();
+    }else if ($method == 'create_follow') {
+        $user = require_login();
+    }else if ($method == 'remove_follow') {
+        $user = require_login();
+    }else if ($method == 'public_queue') {
+
+    }else if ($method == 'user_queue') {
+        $user = require_login();
+    }else if ($method == 'mentions') {
+        $user = require_login();
     }
 
 
